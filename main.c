a87b6c260b03317f2ee15a8705bd64fa58901e6b
Jacob Bush <jacob.k.bush@uon.edu.au>
2019-04-09 03:36:40 +0000

    TRying again to get it to work but not understanding as to why it works when only having the word go into the array but doesnt work when the rest of the code is added

--- main.c
+++ main.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-int Encode(int y);
-int Decode(int y);
+int Encode();
+int Decode();
 
 int main()
 {
-    
     int choice; //whether the word is going to be encrypted or decrypted
-    int key;
-    char word[255]; //the word that needs to be encrypted or decrypted
-printf("enter word to be encrypted or decrypted\n");
-scanf("%s", &*word); //trying to save it in a array 
+    int key;  
+    char word[1024]; //the word that needs to be encrypted or decrypted
+    char tempword;  //temporarily saving the word outside the array
     
-    printf("what is the key?\n");
+    printf("enter word to be encrypted or decrypted\n");
+    scanf("%s", &tempword); //trying to save it in a array 
+    
+    word[1024] = tempword;  //transfering to the array
+    
+    printf("%d\n", word[1]);
+    
+    printf("what is the key?\n");  //key input
     scanf("%d", &key);
     
     do
-    {printf("Decode(1) or Encode(2)\n"); //the decode or enode choice
+    {printf("Decode(1) or Encode(2)\n"); //the decode or encode choice
     scanf("%d", &choice);
     
-   switch (choice){
-    case 1  : printf("%d", Decode(*word));
+    switch (choice) //making sure the choice is 1 or 2
+    { 
+        case 1  : printf("%d", Decode(word));
                 return 0;
-    case 2  : printf("%d", Encode(*word));
+        case 2  : printf("%d", Encode(word));
                 return 0;
-    default : (printf("please insert 1 or 2\n"));
-    
+        default : (printf("please insert 1 or 2\n"));
+    }
 
-}
-
-} while ("choice =! 2 || choice =! 1");
+    }   while ("choice =! 2 || choice =! 1");
 
     
     return 0; 
@@ -37,6 +42,7 @@ scanf("%s", &*word); //trying to save it in a array
 
 int Encode(int y)
 {
+    printf("Encode\n");
     y=0;
     
     return y;
@@ -44,6 +50,7 @@ int Encode(int y)
 
 int Decode(int y)
 {
+    printf("Decode\n");
     y = 0;
     return y;
 }
