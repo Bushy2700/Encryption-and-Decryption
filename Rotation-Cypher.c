0c04c74a398c780915ae5bb0bdb3c1436ca1aa65
Jacob Bush <jacob.k.bush@uon.edu.au>
2019-04-25 11:00:02 +0000

    Optimising it so it will work properly

---
+++ Rotation-Cypher.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <string.h>
+
+int Encryption(int letter, int key);
+int Decryption(int letter, int key);
+
+int main()
+{
+    int UnknownDecryptionNumber;  // The counter that is used to brute force the roation cypher
+    int choice; //whether the word is going to be encrypted or decrypted
+    int key; // a number between 1 and 25 that will change the sentence
+    char Word[1024]; //the word that needs to be encrypted or decrypted
+    int Number = 0; // The general Counter used whenever a counter is needed
+    FILE *Message;
+    FILE *Output;
+    
+    Output = fopen("Output", "w");
+    printf("enter word to be encrypted or decrypted in Message file\n"); 
+    
+    printf("Enter the key (must be between 1 and 25)\n");  //key input
+    do
+    {
+        scanf("%d", &key);}
+    while 
+    Message = fopen("Message", "r");
+    
+    while (Number < 1024)
+    {
+        fscanf(Message, "%c", &Word[Number]);
+        Number++;
+    }
+
+    for (Number = 0; Word[Number] != 0; Number++)
+        //fprintf(Output ,"%d\n", (int)Word[Number]);
+        if ((int)Word[Number + 1] < 0)
+            Word[Number + 1] = 0;
+    do
+    {
+        printf("Decode(0) or Encode(1)\n"); //the decode or encode choice
+        scanf("%d", &choice);
+        Number = 0;
+        if (choice == 0) 
+            {printf("Unknown key(1) or Known key(0)\n");
+            scanf("%d", &choice);
+            if (choice == 1)
+                {key = 1;
+                for (UnknownDecryptionNumber = 0; UnknownDecryptionNumber < 26; UnknownDecryptionNumber++)
+                    {for(Number = 0; Number < 1024; Number++){
+                    Word[Number] = Decryption((int)Word[Number], key);
+                    }
+                fprintf(Output,"\n\n---------------------------------------------------------------------\n\n%s", Word);}
+                return 0;
+                }
+            else if (choice == 1)
+                {for(Number = 0; Number < 1024; Number++)
+                    Word[Number] = Decryption((int)Word[Number], key);
+                fprintf(Output,"%s", Word);
+                return 0;}
+            }
+
+
+        else if (choice == 1) {
+            for (Number = 0; Number < 1024; Number++){
+                Word[Number] = Encryption((int)Word[Number], key);
+                }
+            fprintf(Output,"%s", Word);
+            return 0;
+            }
+        else
+        (printf("please insert 1(Decode) or 2(Encode)\n"));
+        
+    }   while ("choice =! 2 && choice =! 1");
+
+    
+    
+    return 0; 
+}
+
+int Encryption(int letter, int key)
+{
+    if (letter < 65)
+        return letter;
+    else if (letter > 90)
+        return letter;
+
+    letter = letter + key;
+
+    if (letter < 65)
+        return (letter + 26);
+    else if (letter > 90)
+        return (letter - 26);
+    else 
+        return letter;
+}
+
+int Decryption(int letter, int key)
+{
+    if (letter < 65)
+        return letter;
+    else if (letter > 90)
+        return letter;
+
+    letter = letter - key;
+
+    if (letter < 65)
+        return (letter + 26);
+    else if (letter > 90)
+        return (letter - 26);
+    else 
+        return letter;
+}
+
+
+
